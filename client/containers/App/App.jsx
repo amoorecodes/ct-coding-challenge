637142f6dd35b791e6eb583cef82db6fdb3d0d65
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-12-11 11:23:55 -0800

    [add] Barebones App container, hook to HTML

---
+++ client/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>this is going to be entire App component.</p>
+    </div>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const parser = require('body-parser')
-const path = require('path')
 
 const app = express()
 const PORT = 3000
@@ -10,7 +9,10 @@ app.use(parser.json())
 app.use(parser.urlencoded({ extended: true }))
 
 //server static files from Public folder
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static('public'))
 
 // start the server
-app.listen(PORT, () => console.log('Server has started on port ', PORT))
+app.listen(PORT, err => {
+  if (err) console.err('There was a problem starting a server.\n', err)
+  console.log('Server has started on port ', PORT)
+})
